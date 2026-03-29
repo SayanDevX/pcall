51697b6d703e407838773489a0b73673a7f036f0
Sayan Biswas <sayan@air.local>
2026-03-30 02:38:39 +0530

    ai labs

---
+++ lab/ai/assignment1/2grade.py
@@ -0,0 +1,12 @@
+marks = eval(input("Enter Marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif 80 <= marks and marks < 90:
+    print("Grade B")
+elif 70 <= marks and marks < 80:
+    print("Grade C")
+elif 60 <= marks and marks < 70:
+    print("Grade D")
+elif 50 <= marks and marks < 60:
+    print("Grade E")
