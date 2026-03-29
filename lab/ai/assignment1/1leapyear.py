571c984cf22f7ead569d5d52e0ba7ee060f0b4c0
Sayan Biswas <sayan@air.local>
2026-03-30 02:55:48 +0530

    fixed leapyear

--- lab/ai/assignment1/1leapyear.py
+++ lab/ai/assignment1/1leapyear.py
@@ -1,6 +1,6 @@
 year = int(input("Enter A Year: "))
 
-if year % 4 or (year % 100 and year % 400):
+if year % 4 == 0 or (year % 100 == 0 and year % 400 ==0):
     print(year, "is a LeapYear.")
 else:
     print(year, "is not a LeapYear.")
